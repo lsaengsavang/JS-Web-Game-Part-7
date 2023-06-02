bbc3ab0b28461f76886db71494b006206c74ce11
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-06-01 18:34:04 -0700

    Added to JS

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-// Create the playable character
 const pc = newPlayableCharacter(100, 110)
 
-// Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-npc.walkEast()
 
-// Create the inventory
+async function moveNPC(){
+  await npc.walkNorth(1400)
+  await npc.walkEast(1200)
+  await npc.walkSouth(300)
+  await npc.walkEast(1500)
+  await npc.walkSouth(1500)
+  await npc.walkWest(2700)
+  await npc.walkNorth(400)
+}
+
+async function moveNPC(){
+  await npc.walkEast(2000)
+  npc.walkSouth()
+}
+moveNPC()
+
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-// Create everything else
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
